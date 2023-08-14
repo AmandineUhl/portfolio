12c6dfff02f5766f53fa4dc1445fd7d6849c8944
AmandineUhl <didine709@hotmail.com>
2023-08-14 11:21:13 +0200

    cards

--- src/javascript/components/Cards.jsx
+++ src/javascript/components/Cards.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 
-function Cards() {
+function Cards(props) {
+  const { title, iconClass, description } = props;
+
   return (
     <div className="card">
       <div className="card_title">
-        <h3>HTML</h3>
-        <i class="fa-brands fa-html5"></i>
+        <h3>{title}</h3>
+        <i className={`fa-brands ${iconClass}`}></i>
       </div>
-      <p>
-        Maîtrise avancée de HTML, le langage de base pour structurer et
-        organiser le contenu des pages web. Je suis capable de créer des mises
-        en page complexes et d'optimiser le code pour une meilleure performance.
-      </p>
-      <a href="">En savoir plus</a>
+      <p>{description}</p>
     </div>
   );
 }
--- src/javascript/layout/Header.jsx
+++ src/javascript/layout/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Header() {
+  
   return (
     <header>
       <nav>
@@ -8,15 +9,15 @@ function Header() {
         <a href="#">Mes compétences</a>
         <a href="#">Mes réalisations</a>
         <a href="#">Contact</a>
-        <i class="fa-solid fa-chevron-down"></i>
+        <i className="fa-solid fa-chevron-down"></i>
       </nav>
       <div className="welcome">
         <div className="welcome_content">
           <p>Bienvenue, je suis</p>
           <h1>Amandine Uhl</h1>
           <p>Développeuse Front-End</p>
-          <i class="fa-brands fa-github"></i>
-          <i class="fa-brands fa-linkedin"></i>
+          <i className="fa-brands fa-github"></i>
+          <i className="fa-brands fa-linkedin"></i>
         </div>
         <img src="pc.png" alt="" />
       </div>
