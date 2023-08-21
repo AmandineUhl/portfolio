0d19a5387c1fc262ff4973a1a5bb7860da877e3b
AmandineUhl <didine709@hotmail.com>
2023-08-21 14:23:39 +0200

    skills

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,20 +7,33 @@ function Projects() {
       title: "Booki",
       imageSrc: "booki.png",
       description: "Lorem ipsum dolor sit amet. Ut ipsum modi vel assumenda dolorum eos ullam numquam. Ut dicta quaerat a dolores illo qui mollitia ratione id repudiandae consequatur aut enim quaerat. Et internos nihil eos nemo voluptatem rem beatae laudantium eos tempore cupiditate aut expedita iure qui temporibus praesentium.",
-      link: "lien_projet1",
+      link: "https://amandineuhl.github.io/booki/",
     },
     {
       title: "Print It",
       imageSrc: "print it.png",
-      description: "Lorem ipsum dolor sit amet. Ut ipsum modi vel assumenda dolorum eos ullam numquam. Ut dicta quaerat a dolores illo qui mollitia ratione id repudiandae consequatur aut enim quaerat. Et internos nihil eos nemo voluptatem rem beatae laudantium eos tempore cupiditate aut expedita iure qui temporibus praesentium.",
-      link: "lien_projet2",
+      description: "l'incontournable carousel en JS, ce projet avait pour but de rendre un site dynamique en ajoutant un carousel sur la banner",
+      link: "https://amandineuhl.github.io/Print-it-JS/",
     },
     {
       title: "Oh my food",
       imageSrc: "ohmyfood.png",
       description: "Lorem ipsum dolor sit amet. Ut ipsum modi vel assumenda dolorum eos ullam numquam. Ut dicta quaerat a dolores illo qui mollitia ratione id repudiandae consequatur aut enim quaerat. Et internos nihil eos nemo voluptatem rem beatae laudantium eos tempore cupiditate aut expedita iure qui temporibus praesentium.",
-      link: "lien_projet3",
+      link: "https://amandineuhl.github.io/oh-my-food/",
     },
+    {
+      title: "Nina Carducci",
+      imageSrc: "Nina Carducci.png",
+      description: "Optimisation SEO du site de Nina Carducci. J'ai utilisé les outils d'audit google light house, wave et Google Rich Snippet pour orienter les améliorations à apporter",
+      link: "https://amandineuhl.github.io/nina_carducci/",
+    },
+    {
+      title: "Kasa",
+      imageSrc: "kasa.png",
+      description: "Ce projet avait pour but la refonte de la plateform Kasa.Mon rôle a consisté à mettre en œuvre les maquettes Figma avec React, en créant des composants et des routes. Mon travail a contribué à moderniser l'expérience utilisateur de cette plateforme de location. ",
+      link: "https://github.com/AmandineUhl/kasa",
+    },
+    
   ];
 
   return (
